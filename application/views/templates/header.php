fa7d8aa774db69ae4a7b735e2f7b5da1fe0eacc4
Chris King <cjk508@york.ac.uk>
2014-02-20 19:50:58 +0000

    navbar created
    Dropdown is currently not working

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,14 +1,46 @@
+<?php
+if (!isset($_COOKIE['viewType'])){
+	//$_SESSION['ViewType'] = $_POST['viewType'];
+	setcookie('viewType', 'null', time()+3600); 
+}
+if (isset($_POST['viewType'])){
+	//$_SESSION['ViewType'] = $_POST['viewType'];
+	setcookie('viewType', $_POST['viewType'], time()+3600); 
+}
+?>
 <html>
 <head>
-<title><?php echo $title ?> - Cook book</title>
-	<?php echo link_tag('assets/css/bootstrap.css'); ?>
-	<script type="text/javascript"
-	src="<?php echo site_url('assets/js/bootstrap.js'); ?>"></script>
+	<title><?php echo $title ?> - Cook book</title>
+	<?php echo link_tag('assets/css/bootstrap.css');
+		  echo link_tag('assets/css/style.css'); ?>
+
+	<script type="text/javascript" src="js/jquery-1.8.0.js"></script>
+	<script type="text/javascript" src="<?php echo site_url('assets/js/bootstrap.js'); ?>"></script>
 </head>
+
 <body>
-<h2>Cooking Website</h2>
-	<ul class="nav nav-pills">
-  <li class="active"><a href="#">Home</a></li>
-  <li><a href="#">Profile</a></li>
-  <li><a href="#">Messages</a></li>
-</ul>
+<header> 
+	<h2 class="brand">Cooking Website</h2>
+	<nav>
+		<ul class="nav nav-pills">
+			<li class="active"><a href="#">Home</a></li>
+			<!-- @todo need to get the drop downs working. Seems to meet the same logic as is on the bootstrap page but it doesn't currently work-->
+			<li class="dropdown">
+				<a data-toggle="dropdown" href="#">Categories<span class="caret"></span></a>
+				<ul class="dropdown-menu" role="menu">
+					Meat
+				</ul>
+			</li>
+		</ul>
+	</nav>
+	
+	<form class="navbar-search pull-right">
+	    <input type="text" class="search-query" placeholder="Search">
+	</form>
+</header>
+<script type="text/javascript">
+	$(document).ready( function() {
+		$('.dropdown-toggle').dropdown()
+	});
+</script>
+<div class = "wrapper">
