a8d16b98612b89024a4b78ca92e826ced883054f
nnikolov91 <Nikola Nikolov>
2014-02-22 16:51:16 +0000

    Changed CSS of site a bit, also worked a bit on #10.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,3 +1,23 @@
+<?php ?>
+<style>
+/**
+
+	This styling is only relevant to the homepage
+	@todo if there is a more efficient way of implementing this style then use that
+
+	*/
+html {
+	/*background: <?php //echo "url('" . base_url('assets/images/backgroundImage.jpg') . "')" ?> no-repeat center center fixed;*/
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+}
+
+body {
+	background: transparent;
+}
+</style>
 <?php 
 /**
 * If the session variable has not been set then show the default welcome screen
@@ -6,33 +26,13 @@
 $sessionData = $this->session->all_userdata();
 if ($sessionData['viewType'] == 'null' /*or $sessionData['viewType'] != 'null'*/){
 ?>
-	<style>
-	/**
 
-	This styling is only relevant to the homepage
-	@todo if there is a more efficient way of implementing this style then use that
-
-	*/
-		html {
-			background: url(assets/images/backgroundImage.jpg) no-repeat center center fixed; 
-			-webkit-background-size: cover;
-			-moz-background-size: cover;
-            -o-background-size: cover;
-			background-size: cover;
-	    }
-	    body{
-	    	background: transparent;
-	    }
-	</style>
-
-	<!-- This is the large welcome box in the middle of the homepage -->
-	<div class="jumbotron jumbotron-homepage">
-		<h1>Welcome to the Cook Book!</h1>
-		<p> 
-			Recipes your way!
-			<br>
-			You can cook anything with our site! It is really easy.
-			<br>
+<!-- This is the large welcome box in the middle of the homepage -->
+<div class="jumbotron jumbotron-homepage">
+	<h1>Welcome to the Cook Book!</h1>
+	<p>
+		Recipes your way! <br> You can cook anything with our site! It is
+		really easy. <br>
 			<?php echo $db_works?>
 		</p>
 		
@@ -42,14 +42,16 @@ if ($sessionData['viewType'] == 'null' /*or $sessionData['viewType'] != 'null'*/
 		<!-- A form that allows the user to select the type of interface they wish to use
 			This is based on how skilled they believe they are at cooking
 		 -->
-		<p>
-			<form class="btn-group" action = '#' method="post">
-				<button class="btn btn-primary" name = "viewType" value = 'Step-by-Step'>Step-by-Step</button>
-				<button class="btn btn-primary" name = "viewType" value = 'Segmented'>Segmented</button>
-				<button class="btn btn-primary" name = "viewType" value = 'Narrative'>Narrative</button>
-			</form>
-		</p>
-	</div>
+	<p>
+	
+	
+	<form class="btn-group" action='#' method="post">
+		<button class="btn btn-primary" name="viewType" value='Step-by-Step'>Step-by-Step</button>
+		<button class="btn btn-primary" name="viewType" value='Segmented'>Segmented</button>
+		<button class="btn btn-primary" name="viewType" value='Narrative'>Narrative</button>
+	</form>
+	</p>
+</div>
 
 
 
@@ -67,81 +69,81 @@ else{
 	echo '<p>'.$sessionData['viewType'].'</p>';
 ?>
 <div class="panel panel-default mostRecent">
-  <div class="panel-heading">
-    <h3 class="panel-title">Most Recent Recipes</h3>
-  </div>
-  <div class="panel-body">
- 	 <!-- @todo load most recent recipes -->
-	  <div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 1</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>
+	<div class="panel-heading">
+		<h3 class="panel-title">Most Recent Recipes</h3>
+	</div>
+	<div class="panel-body">
+		<!-- @todo load most recent recipes -->
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 1</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
 
-	  <div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 2</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 2</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
 
-	  <div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 3</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 3</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
 
 
-  </div>
+	</div>
 </div>
 
 <div class="panel panel-default mostPopular pull-right">
-  <div class="panel-heading">
-    <h3 class="panel-title">Most Popular Recipes</h3>
-  </div>
-  <div class="panel-body">
-  <!-- @todo load most popular recipes -->
-  	<div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 1</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>
+	<div class="panel-heading">
+		<h3 class="panel-title">Most Popular Recipes</h3>
+	</div>
+	<div class="panel-body">
+		<!-- @todo load most popular recipes -->
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 1</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
 
-	  <div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 2</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 2</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
 
-	  <div class="media">
-	    <a class="pull-left" href="#">
-	      <img class="media-object" src="assets/images/64test.svg" alt="">
-	    </a>
-	    <div class="media-body">
-	      <h4 class="media-heading">Recipe 3</h4>
-	      Lorem Ipsum Ingredients
-	    </div>
-	  </div>	
-  </div>
+		<div class="media">
+			<a class="pull-left" href="#"> <img class="media-object"
+				src="assets/images/64test.svg" alt="">
+			</a>
+			<div class="media-body">
+				<h4 class="media-heading">Recipe 3</h4>
+				Lorem Ipsum Ingredients
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php 
--- application/views/recipes/view.php
+++ application/views/recipes/view.php
@@ -1,26 +1,77 @@
 
-<h2><?php echo  $recipe_item->getTitle()?></h2>
-<p><?php var_dump( $recipe_item->getCategory())?></p>
 
-<?php
-foreach ( $recipe_item->getIngredientPools () as $pool ) {
-	?>
-<h2><?php echo $pool-> getDifficulty()?></h2>
-<ul>
-
-<?php
+<script type="text/javascript">
 	
-	foreach ( $pool->getIngredients () as $ingredient ) {
-		?>
+</script>
+
+<div id="recipe-contents">
+	<div class="panel panel-default" id="main-panel">
+		<h1><?php echo  $recipe_item->getTitle()?></h1>
+		<form class="btn-group" action='#' method="post">
+			<button class="btn btn-primary" name="viewType" value='Step-by-Step'>Step-by-Step</button>
+			<button class="btn btn-primary" name="viewType" value='Segmented'>Segmented</button>
+			<button class="btn btn-primary" name="viewType" value='Narrative'>Narrative</button>
+		</form>
+
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
+				<div class="panel-heading ">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion"
+							href="#collapseOne">Ingredients</a>
+					</h4>
+				</div>
+				<div id="collapseOne" class="panel-collapse collapse in">
+					<div class="panel-body">
+						<img alt="test"
+							src="<?php echo base_url('assets/images/')."/".$recipe_item->getImage()?>">
+						<p>
+							<?php
+							foreach ( $recipe_item->getIngredientPools () as $pool ) {
+								?>
+
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						<h3><?php echo $pool-> getDifficulty()?></h3>
+						<ul>
+
+<?php
+								
+								foreach ( $pool->getIngredients () as $ingredient ) {
+									?>
 		<li><?php echo $ingredient?></li>
 		<?php
-	}
-	?>
+								}
+								?>
 	</ul>
 <?php
-}
-?>
+							}
+							?>
 
-<p><?php echo $recipe_item->getNarrativeMethod() ?></p>
-<p><?php var_dump($recipe_item->getSegmentedMethod()) ?></p>
+					</div>
+				</div>
+			</div>
+			<p>
+	<?php echo $recipe_item->getNarrativeMethod()?>
+	
+	</p>
+		</div>
 
+
+
+	</div>
+</div>
