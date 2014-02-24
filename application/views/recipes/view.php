0f8886da2fde7f5f7e9344876a3ee0bcd4730361
nnikolov91 <Nikola Nikolov>
2014-02-24 16:16:36 +0000

    Fixed error in recipes_view - updated workbench model.

--- application/views/recipes/view.php
+++ application/views/recipes/view.php
@@ -1,23 +1,48 @@
 
+<script type="text/javascript"
+	src="<?php echo base_url('assets/js/jquery.totemticker.js'); ?>"></script>
 
 <script type="text/javascript">
-	
+$(document).ready(function(){
+
+	$('#collapseOne').on('hidden.bs.collapse', function () {
+		  $("#preparation").css({"max-height": "60%"});
+	}); 
+	$('#collapseOne').on('shown.bs.collapse', function () {
+		  $("#preparation").css({"max-height": "30%"});
+	}); 
+});
 </script>
+
 
 <div id="recipe-contents">
 	<div class="panel panel-default" id="main-panel">
-		<h1><?php echo  $recipe_item->getTitle()?></h1>
+		<h3><?php echo  $recipe_item->getTitle()?> <small><?php foreach ($recipe_item->getCategory() as $cat) {
+			echo $cat . " "; 
+		}?></small></h3>
 		<form class="btn-group" action='#' method="post">
 			<button class="btn btn-primary" name="viewType" value='Step-by-Step'>Step-by-Step</button>
 			<button class="btn btn-primary" name="viewType" value='Segmented'>Segmented</button>
 			<button class="btn btn-primary" name="viewType" value='Narrative'>Narrative</button>
 		</form>
-
+		<?php
+		$sessionData = $this->session->all_userdata ();
+		if ($sessionData ['viewType'] == 'null') {
+			?>
+			<p>No user type</p>
+		<?php
+		} else {
+			?>
+			<p><?php echo $sessionData['viewType'] ?></p>
+		<?php
+		}
+		?>
 		<div class="panel-group" id="accordion">
 			<div class="panel panel-default">
 				<div class="panel-heading ">
 					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion"
+						<span class="glyphicon glyphicon-arrow-up"></span> <a
+							data-toggle="collapse" data-parent="#accordion"
 							href="#collapseOne">Ingredients</a>
 					</h4>
 				</div>
@@ -29,28 +54,9 @@
 							<?php
 							foreach ( $recipe_item->getIngredientPools () as $pool ) {
 								?>
-
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
 						<h3><?php echo $pool-> getDifficulty()?></h3>
 						<ul>
-
 <?php
-								
 								foreach ( $pool->getIngredients () as $ingredient ) {
 									?>
 		<li><?php echo $ingredient?></li>
@@ -65,13 +71,53 @@
 					</div>
 				</div>
 			</div>
-			<p>
-	<?php echo $recipe_item->getNarrativeMethod()?>
-	
-	</p>
+
+			
+			<?php
+			if ($sessionData ['viewType'] == 'null' or $sessionData ['viewType'] == 'Narrative') {
+				?>
+			<div class="panel panel-default" id="preparation" data-spy="scroll"
+				data-target=".navbar-example">
+				<div class="panel-heading navbar-example">
+					<h3 class="panel-title">Preparation</h3>
+					<p><?php echo $recipe_item-> getNarrativeMethod()?>
+					<p><?php echo $recipe_item-> getNarrativeMethod()?>
+					<p><?php echo $recipe_item-> getNarrativeMethod()?>
+					<p><?php echo $recipe_item-> getNarrativeMethod()?>
+					<p><?php echo $recipe_item-> getNarrativeMethod()?>
+				</div>
+			</div>
+					
+				<?php } else { ?>
+					<div class="panel panel-default" id="preparation" data-spy="scroll"
+				data-target=".navbar-example">
+				<div class="panel-heading navbar-example">
+					<h3 class="panel-title">Preparation</h3>
+					<ol id="ticker">
+				
+					<?php foreach ($recipe_item->getSegmentedMethod() as $step) {?>
+					
+						<li><?php echo $step ?></li>
+					<?php
+				}
+				?>
+					<?php foreach ($recipe_item->getSegmentedMethod() as $step) {?>
+					
+						<li><?php echo $step ?></li>
+					<?php
+				}
+				?>
+					<?php foreach ($recipe_item->getSegmentedMethod() as $step) {?>
+					
+						<li><?php echo $step ?></li>
+					<?php
+					}
+					?>
+					</ol>
+				</div>
+			</div>
+				<?php }
+			?>
 		</div>
-
-
-
 	</div>
 </div>
