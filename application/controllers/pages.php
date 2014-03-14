40090348c22f5838675ba057b1790c85dc86204b
Chris King <cjk508@york.ac.uk>
2014-03-14 09:30:50 +0000

    Revert 831166b..8bb1684
    This rolls back to commit 831166b6a6f184966146cef55d48d9aeaae6c403.

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -13,14 +13,12 @@ class Pages extends CI_Controller {
 		}
 		
 		$data ['title'] = ucfirst ( $page ); // Capitalize the first letter
-
 		$data['db_works'] = $this->recipes_model->test_db();
 		$data ['mostRecents'] = $this->recipes_model->get_latest();
 		$data['headerSurprise'] = $this->recipes_model->get_surprise();
 		$data['headerCategories'] = $this->recipes_model->get_all_categories();
 		$data ['AllItems'] = $this->recipes_model->get_all_recipes_AZ();
-
-
+		
 		$this->load->view ( 'templates/header', $data );
 		$this->load->view ( 'pages/' . $page, $data );
 		$this->load->view ( 'templates/footer', $data );
