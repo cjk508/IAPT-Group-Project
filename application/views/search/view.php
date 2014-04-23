1c4ae2fd08d3344e775fb227469711f309d39919
Chris King <cjk508@york.ac.uk>
2014-04-23 13:11:51 +0100

    filtering now on search page

--- application/views/search/view.php
+++ application/views/search/view.php
@@ -1,8 +1,13 @@
-<div class="panel panel-default">
+<?php 
+	$CookingTimes = array();
+	$Servings = array();
+	$AllCategories = array();
+?>
+<div class="panel panel-default  category-box pull-right">
 	<div class="panel-heading">
-		<h3 class="panel-title">Search Results</h3>
+		<h3 style = "margin-bottom: 0px;">Search Results</h3>
 	</div>
-	<div class="panel-body category-box">
+	<div class="panel-body">
 
 		<ul class="media-list">
 <?php if(is_null($searchValues) == TRUE){
@@ -10,7 +15,17 @@
 		}
 		else{
 			foreach($searchValues as $searchValue) {?>
-			<li class="media recipe_list"><a class="pull-left"
+			<li class="media recipe_list <?php
+				$tempAllCategories = $searchValue->getCategory();
+				foreach ($tempAllCategories as $tempAllCategory) {
+					echo $tempAllCategory->getCategoryName(). ' ';
+				}
+				echo ' Serves'. $searchValue->getServings() .' Time' . $searchValue->getRecipeCookTime();
+				
+				array_push($CookingTimes, $searchValue->getRecipeCookTime());
+				array_push($AllCategories, $tempAllCategory->getCategoryName() );
+				array_push($Servings, $searchValue->getServings());
+			?>"><a class="pull-left"
 				href="<?php echo site_url('recipe/'.$searchValue->getID()); ?>"> <img
 					class="category-media-object"
 					src="<?php echo base_url('assets/images/')."/".$searchValue->getImage();?> "
@@ -53,3 +68,148 @@
 			<?php } ?>
 	</div>
 </div>
+<div class="panel panel-default filter-box">
+	<div class = "panel-heading"> 
+		<h3 style ="margin-bottom: 0px;">Filtering Options</h3>
+	</div>
+	<?php 
+		//Remove all duplicate values for the array, leaving just one instance of each filtering option
+		$AllCategories = array_unique($AllCategories);
+		$Servings = array_unique($Servings);
+		$CookingTimes = array_unique($CookingTimes);
+	?>
+	<div class = "panel-body ">
+		<h4>Category</h4>
+		<ul class="list-group">
+			<?php foreach ($AllCategories as $AllCategory) {?>
+				<div class="input-group">
+				      <span class="input-group-addon">
+				        <input type="checkbox" value = "<?php echo $AllCategory; ?>" onclick="toggleFilter('<?php echo $AllCategory; ?>', this)"> <?php echo $AllCategory; ?>
+				      </span>
+				</div>
+			<?php } ?>
+		</ul>
+		<h4>Servings</h4>
+		<ul class="list-group">
+			<?php foreach ($Servings as $Serving) {?>
+				<div class="input-group">
+				      <span class="input-group-addon">
+				        <input type="checkbox" value = "<?php echo 'Serves'.$Serving; ?>" onclick="toggleFilter('<?php echo 'Serves'.$Serving; ?>', this)"> <?php echo $Serving; ?>
+				      </span>
+				</div>
+			<?php } ?>
+		</ul>
+		<h4>Prep Time</h4>
+		<ul class="list-group">
+			<?php foreach ($CookingTimes as $CookingTime) {?>
+				<div class="input-group">
+				      <span class="input-group-addon">
+				        <input type="checkbox" value = "<?php echo 'Time'.$CookingTime; ?>" onclick = "toggleFilter('<?php echo 'Time'.$CookingTime;?>', this)"> <?php echo $CookingTime. " minutes"; ?>
+				      </span>
+				</div>
+			<?php } ?>
+		</ul>
+	</div>
+</div>	
+
+<script type="text/javascript">
+var categoryLog = new Array();
+var servesLog = new Array();
+var timeLog = new Array();
+var checkedLog = new Array();
+function toggleFilter(className, checkBoxObject){
+	if (checkBoxObject.checked){
+		if (checkBoxObject.value.indexOf( "Serves") > -1){
+			servesLog.push(checkBoxObject.value);	
+		}
+		else if (checkBoxObject.value.indexOf("Time") > -1){
+			timeLog.push(checkBoxObject.value);	
+		}
+		else{
+			categoryLog.push(checkBoxObject.value);	
+		}
+	}
+	else{
+		removeValueFromLog(checkBoxObject.value);	
+	}
+
+	var elements = document.getElementsByClassName("recipe_list");
+	for (var i = 0; i < elements.length; i++) {
+		filterItem(elements[i]);
+	}	
+}
+
+function filterItem(recipe){
+	var classList = recipe.className.split(/\s+/);
+	var visible = [false,false,false];
+	for (var i = classList.length - 1; i >= 0; i--) {
+		if (categoryLog.length > 0) {	
+			for (var j = categoryLog.length - 1; j >= 0; j--) {	
+				if (categoryLog[j] == classList[i]){
+					visible[0] = true;
+				}
+			}
+		}
+		else {
+			visible[0] = true;
+		}
+		if (servesLog.length > 0) {	
+			for (var j = servesLog.length - 1; j >= 0; j--) {	
+				if (servesLog[j] == classList[i]){
+					visible[1] = true;
+				}
+			}
+		}
+		else {
+			visible[1] = true;
+		}
+		if (timeLog.length > 0) {	
+			for (var j = timeLog.length - 1; j >= 0; j--) {	
+				if (timeLog[j] == classList[i]){
+					visible[2] = true;
+				}
+			}
+		}
+		else {
+			visible[2] = true;
+		}
+	}
+	if (visible[0] == true && visible[1] == true && visible[2] == true){
+		$(recipe).removeClass("hidden");
+	}
+	else{
+		if (categoryLog.length == 0 && servesLog.length == 0 && timeLog.length == 0 ){
+			$(recipe).removeClass("hidden");
+		}
+		else{
+			$(recipe).addClass("hidden");	
+		}
+	}
+}
+
+function removeValueFromLog(cbValue){
+	var deletedOne = false;
+	for (var i = categoryLog.length - 1; i >= 0; i--) {
+		if (categoryLog[i] == cbValue && deletedOne == false){
+			categoryLog.splice(i, 1);
+			deletedOne = true;
+		}
+	};
+	if (deletedOne == false){
+		for (var i = servesLog.length - 1; i >= 0; i--) {
+			if (servesLog[i] == cbValue && deletedOne == false){
+				servesLog.splice(i, 1);
+				deletedOne = true;
+			}
+		};
+		if (deletedOne == false){
+			for (var i = timeLog.length - 1; i >= 0; i--) {
+				if (timeLog[i] == cbValue && deletedOne == false){
+					timeLog.splice(i, 1);
+					deletedOne = true;
+				}
+			};
+		}
+	}
+}
+</script>
