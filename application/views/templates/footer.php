d7f2ef03777679af17e32131f77465ffab890ded
nnikolov91 <Nikola Nikolov>
2014-04-30 00:08:48 +0100

    footer update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,7 @@
 <!-- Wrapper class end -->
 </body>
 <footer>
-	<strong>&copy; IAPT Module, Group 6, 2014. </strong> <a
+	<strong style="margin-left: -80px; ">&copy; IAPT Module, Group 6, 2014. </strong> <a class="pull-left"
 		style="font-size: 18px;margin-top: 0px;margin-left: 10px;" href="<?php echo base_url()?>help">Help Section</a>
 		<span class = "pull-right"> <a href = "<?php echo  base_url().'documentation/LogBook-Group6.pdf';?>"> Log Book  </a> <a href = "<?php echo  base_url(). 'documentation/Interactive-System-Quality-Reporting-Group6.pdf';?>">Documentation</a>   </span>
 </footer>
@@ -12,8 +12,6 @@ function keyUp_Search(keyValue){
 	if (keyValue.keyCode == 83){
 		document.getElementById("search").focus();
 	}
-
-
 }
 document.addEventListener('keyup', keyUp_Search, false);
 </script>
