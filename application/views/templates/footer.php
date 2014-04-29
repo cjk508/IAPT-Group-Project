8a24307f6d4c1a3fe1a64a85511f7026c59f7f12
Chris King <cjk508@york.ac.uk>
2014-04-30 00:06:00 +0100

    Added documentation and links
    Links  to the documentation in the footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,6 +4,7 @@
 <footer>
 	<strong>&copy; IAPT Module, Group 6, 2014. </strong> <a
 		style="font-size: 18px;margin-top: 0px;margin-left: 10px;" href="<?php echo base_url()?>help">Help Section</a>
+		<span class = "pull-right"> <a href = "<?php echo  base_url().'documentation/LogBook-Group6.pdf';?>"> Log Book  </a> <a href = "<?php echo  base_url(). 'documentation/Interactive-System-Quality-Reporting-Group6.pdf';?>">Documentation</a>   </span>
 </footer>
 
 <script>
